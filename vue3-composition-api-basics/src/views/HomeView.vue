342b2b5beb53ac27e5a76a86c5f70112b3a9b742
Usman Liaqat <chusmanliaqat1020@gmail.com>
2022-11-11 09:32:23 +0500

    first commit

---
+++ vue3-composition-api-basics/src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+<!-- Composition APi -->
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0)
+
+const increaseCounter = () => {
+  counter.value++
+}
+
+const decreaseCounter = () => {
+  if (counter.value > 0) {
+    counter.value--
+  }
+}
+</script>
+
+<!-- Option Api -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      if (this.counter > 0) {
+        this.counter--
+      }
+    }
+  }
+}
+</script> -->
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
