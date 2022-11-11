7d425557cec33ff024436f2de40c09d9be716461
Usman Liaqat <chusmanliaqat1020@gmail.com>
2022-11-11 15:24:22 +0500

    Initial - Commit

--- 02 - Refs, Reactive Objects _ Non-Reactive Data/src/views/HomeView.vue
+++ 02 - Refs, Reactive Objects _ Non-Reactive Data/src/views/HomeView.vue
@@ -1,76 +1,43 @@
 <template>
   <div class="home">
+    <h3>{{ counterData.title }}:</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
     </div>
   </div>
 </template>
 
-<!-- Composition APi -->
-<!-- way 1 -->
-<!-- <script>
-import { ref } from 'vue'
-export default {
-  setup() {
-    const counter = ref(0)
-
-    const increaseCounter = () => {
-      counter.value++
-    }
-
-    const decreaseCounter = () => {
-      if (counter.value > 0) {
-        counter.value--
-      }
-    }
-
-    return {
-      counter, increaseCounter, decreaseCounter
-    }
-  }
-}
-</script> -->
-
-<!-- way 2 -->
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
-const counter = ref(0)
+// ref
+// const counter = ref(0),
+// const counterTitle = ref('My Counter')
+
+// reactive
+const counterData = reactive({
+  count: 0,
+  title: "My Counter"
+})
 
 const increaseCounter = () => {
-  counter.value++
+  counterData.count++
 }
 
 const decreaseCounter = () => {
-  if (counter.value > 0) {
-    counter.value--
+  if (counterData.count > 0) {
+    counterData.count--
   }
 }
 </script>
-
-
-<!-- Option Api -->
-<!-- <script>
-export default {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++
-    },
-    decreaseCounter() {
-      if (this.counter > 0) {
-        this.counter--
-      }
-    }
-  }
-}
-</script> -->
 
 <style scoped>
 .home {
@@ -82,5 +49,9 @@ export default {
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
 }
 </style>
