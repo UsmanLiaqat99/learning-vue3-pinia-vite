71f149b1c132fa3ae40e3974b83046fbf6cd2da1
Usman Liaqat <chusmanliaqat1020@gmail.com>
2022-11-12 22:49:21 +0500

    Initial - Commit

---
+++ 07 - Lists, Teleport, Template Refs _ nextTick/src/views/ModalsView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="modals">
+        <h1>Modals</h1>
+
+        <button @click="showModal = true">Show Modal</button>
+
+        <teleport to=".modals-container">
+            <div class="modal" v-if="showModal">
+                <h1>This is a Modal</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quos ipsa
+                    sapiente neque autem. Eveniet, deleniti quae? Dolor fugit facere
+                    blanditiis excepturi repellat adipisci officiis voluptate et autem
+                    consectetur! A?
+                </p>
+                <button @click="showModal = false">Hide Modal</button>
+            </div>
+        </teleport>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+
+const showModal = ref(false);
+</script>
+
+<style>
+.modal {
+    background-color: beige;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
---
+++ 07 - Lists, Teleport, Template Refs _ nextTick/src/views/PostsView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about">
+    <h1>Posts</h1>
+
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <router-link :to="`/postDetail/${post.id}`">{{ post.title }}</router-link>
+      </li>
+    </ul>
+
+    <input type="text" v-focus>
+  </div>
+</template>
+
+<script setup>
+import { vFocus } from '../directives/vAutoFocus'
+import { ref } from 'vue'
+
+const posts = ([
+  {
+    id: 'id1',
+    title: 'Post 1'
+  },
+  {
+    id: 'id2',
+    title: 'Post 2'
+  },
+  {
+    id: 'id3',
+    title: 'Post 3'
+  }
+])
+
+</script>
+
+<style scoped>
+ul {
+  margin: 20px 0px;
+}
+</style>
