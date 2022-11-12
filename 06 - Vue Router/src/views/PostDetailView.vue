4e4175f6db58ad37eeaead369c472b7fc78bfab0
Usman Liaqat <chusmanliaqat1020@gmail.com>
2022-11-12 14:31:33 +0500

    Initial - Commit

---
+++ 06 - Vue Router/src/views/PostDetailView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="about">
+    <h1>This is a detail page</h1>
+    <p>Display the content of page with ID of <span>{{ $route.params.id }}</span>
+      here!</p>
+
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
+
+    <div>
+      <button @click="goToFirstPost">Go to first Post</button>
+    </div>
+
+    <div>
+      <button @click="goHomeIn3Sec">Go Home in 3 Seconds</button>
+    </div>
+
+    <p>
+      <router-link to="/posts">&lt; Back</router-link>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const showPostId = () => {
+  alert(`The ID of this Post is: ${route.params.id}`)
+}
+
+const goHomeIn3Sec = () => {
+  setTimeout(() => {
+    router.push('/')
+  }, 3000)
+}
+
+const goToFirstPost = () => {
+  router.push({
+    name: 'postDetail',
+    params: {
+      id: 'id1'
+    }
+  })
+}
+
+</script>
+
+<style scoped>
+span {
+  color: green;
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
---
+++ 06 - Vue Router/src/views/PostsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/id1">Post 1</router-link>
+      </li>
+      <li>
+        <router-link to="/postDetail/id2">Post 2</router-link>
+      </li>
+      <li>
+        <router-link to="/postDetail/id3">Post 3</router-link>
+      </li>
+    </ul>
+
+    <input type="text" v-focus>
+  </div>
+</template>
+
+<script setup>
+import { vFocus } from '../directives/vAutoFocus'
+</script>
+
+<style scoped>
+ul {
+  margin: 20px 0px;
+}
+</style>
