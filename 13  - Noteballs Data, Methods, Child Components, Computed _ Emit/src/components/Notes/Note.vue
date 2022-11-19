d0b77f2989eabba46b46e5ac28289fc3adfa5de0
UsmanLiaqat <chusumanliaqat1020@gmail.com>
2022-11-19 13:03:03 +0500

    Initial - Commit

---
+++ 13  - Noteballs Data, Methods, Child Components, Computed _ Emit/src/components/Notes/Note.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      {{ note.content }}
+      <div class="has-text-right has-text-grey-light mt-2">
+        <small>{{
+          note.content.length > 1
+            ? note.content.length + " characters"
+            : note.content.length + " character"
+        }}</small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item" @click="handleDeleteClick">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+// Props
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+});
+
+// Emits
+const emit = defineEmits(["deleteClicked"]);
+
+// Handle Delete Click
+const handleDeleteClick = () => {
+  emit("deleteClicked", props.note.id);
+};
+</script>
---
+++ 14 - Noteballs Pinia, Reusable Components, Router _ More/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavBar />
+  <div class="container is-max-desktop py-4">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// imports
+import NavBar from "@/components/Layout/NavBar.vue";
+</script>
+
+<style>
+@import "bulma/css/bulma.min.css";
+</style>
