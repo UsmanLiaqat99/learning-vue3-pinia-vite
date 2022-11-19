6d4d9c296fa5d2bbacb2993f1372b8267d7fa61d
UsmanLiaqat <chusumanliaqat1020@gmail.com>
2022-11-19 12:19:40 +0500

    Initial - Commit

--- 13  - Noteballs Data, Methods, Child Components, Computed _ Emit/src/views/ViewNotes.vue
+++ 13  - Noteballs Data, Methods, Child Components, Computed _ Emit/src/views/ViewNotes.vue
@@ -3,25 +3,31 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add new Note
           </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" :key="i" class="card mb-4">
+    <div v-for="note in notes" :key="note.id" class="card mb-4">
       <div class="card-content">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus qui id
-        aliquid magnam odio distinctio laudantium delectus dolores asperiores
-        facere? Explicabo accusamus veritatis dolor iste ad eaque quas non
-        laboriosam?
+        {{ note.content }}
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
@@ -30,3 +36,37 @@
     </div>
   </div>
 </template>
+
+<script setup>
+// Imports
+import { ref } from "@vue/reactivity";
+
+// Notes
+const newNote = ref("");
+const newNoteRef = ref(null);
+
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus qui id aliquid magnam odio distinctio laudantium delectus dolores asperiores facere? Explicabo accusamus veritatis dolor iste ad eaque quas non laboriosam?",
+  },
+  {
+    id: "id2",
+    content: "This is a shorter note! WOO!",
+  },
+]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime(),
+    id = currentDate.toString();
+  let note = {
+    id: id,
+    content: newNote.value,
+  };
+
+  notes.value.unshift(note);
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
+</script>
